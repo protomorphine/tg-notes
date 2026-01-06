de8f0e5437ac61a68388d4ea42223fd0dcb2f8f3
Daniil Zaytsev <kupernewfag@gmail.com>
2026-01-06 23:34:25 +0300

    add middlewares to generate request ID and log requests

---
+++ internal/bot/middleware/middleware.go
@@ -0,0 +1,63 @@
+// Package middleware provides midllewares for telegram bot
+package middleware
+
+import (
+	"context"
+	"log/slog"
+	"time"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+	"github.com/google/uuid"
+)
+
+type middlewareCtxKey string
+
+const reqIDKey middlewareCtxKey = "reqID"
+
+func NewReqID() bot.Middleware {
+	return func(next bot.HandlerFunc) bot.HandlerFunc {
+		return func(ctx context.Context, bot *bot.Bot, update *models.Update) {
+			existingID := GetReqID(ctx)
+			if existingID == uuid.Nil {
+				reqID := uuid.New()
+
+				next(context.WithValue(ctx, reqIDKey, reqID), bot, update)
+				return
+			}
+
+			next(ctx, bot, update)
+		}
+	}
+}
+
+func NewLog(logger *slog.Logger) bot.Middleware {
+	return func(next bot.HandlerFunc) bot.HandlerFunc {
+		logger := logger.With(slog.String("component", "middleware/log"))
+
+		return func(ctx context.Context, b *bot.Bot, update *models.Update) {
+			entry := logger.With(
+				slog.String("username", update.Message.From.Username),
+				slog.Int64("chatID", update.Message.Chat.ID),
+				slog.String("reqID", GetReqID(ctx).String()),
+			)
+
+			entry.Info("request accepted")
+
+			t1 := time.Now()
+			next(ctx, b, update)
+
+			entry.Info("request completed", slog.String("duration", time.Since(t1).String()),)
+		}
+	}
+}
+
+func GetReqID(ctx context.Context) uuid.UUID {
+	entry := ctx.Value(reqIDKey)
+
+	if reqID, ok := entry.(uuid.UUID); ok {
+		return reqID
+	}
+
+	return uuid.Nil
+}
